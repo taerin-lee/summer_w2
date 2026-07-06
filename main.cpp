86db1b78244d7c4585d95ad732d007c81ac38ec4
taerin-lee <flsxodl@ewhain.net>
2026-07-06 02:49:30 +0000

    lab5

--- main.cpp
+++ main.cpp
@@ -2,26 +2,40 @@
 #include "timeOfDay.h"
 #include "alarm.h"
 
-namespace LeeTaerin2276249 {
-    bool compareTimeOfDay(const timeOfDay& t1, const timeOfDay& t2) {
-        return (t1.getHour() == t2.getHour()) && (t1.getMinute() == t2.getMinute());
-    }
-}
-
 int main() {
-    using namespace LeeTaerin2276249; 
-    
-    alarm a1; 
-    a1.print();
-    
-    alarm a2{timeOfDay{11, 44}, true};
-    a2.print(); 
-    
-    if (compareTimeOfDay(a1.getWakeTime(), a2.getWakeTime())) {
-        std::cout << "same\n";
+
+    using namespace LeeTaerin2276249;
+    using namespace std;
+
+    timeOfDay t1, t2;
+    // 1. 객체 2개를 입력연산자(>>)를 이용해서 표준스트림으로 입력
+    // (터미널에 입력할 때 예: 10 20 엔터 치면 10시 20분으로 들어감)
+    std::cin>> t1;
+    std::cin>> t2;
+
+    // 2. 출력연산자(<<)를 이용해서 표준스트림으로 출력
+    cout << t1 << "\n";
+    cout << t2 << "\n";
+    // std::cout <<t1 <<" "<< t2 <<'\n';
+
+    // 3. 두번째 객체에 전위증가연산자(++) 적용한 것을 표준스트림으로 출력
+    cout << ++t2 << "\n";
+
+    // 4. 두번째 객체에 후위증가연산자(++) 적용한 것을 표준스트림으로 출력
+    cout << t2++ << "\n";
+
+    // 5. 두번째 객체를 표준스트림으로 출력 (후위증가 이후 반영된 결과 확인용)
+    cout << t2 << "\n";
+
+    // 6. 두 객체를 비교연산자(==)를 이용하여 비교
+    if (t1 == t2) {
+        cout << "same\n";
     } else {
-        std::cout << "different\n";
+        cout << "different\n";
     }
 
+    // 7. 두 객체를 +이항연산자를 이용하여 더한 값을 표준스트림으로 출력
+    cout << (t1 + t2) << "\n";
+    // std::cout << t1+t2 <<std::endl;
     return 0;
 }
--- timeOfDay.h
+++ timeOfDay.h
@@ -32,9 +32,9 @@ namespace LeeTaerin2276249 {
         void input() {
             int h, m;
             std::cout << "Enter hour(0~23): ";
-            std::cin >> h;  // 👈 제 오타를 정상적으로 수정했습니다!
+            std::cin >> h;
             std::cout << "Enter minute(0~59): ";
-            std::cin >> m;  // 👈 제 오타를 정상적으로 수정했습니다!
+            std::cin >> m;
             
             testHour(h);
             testMinute(m);
@@ -63,4 +63,90 @@ namespace LeeTaerin2276249 {
         int getMinute() const { return minute; }
     };
 
+    // 1. 전위 증가 연산자 (++t) : 1분을 먼저 더하고 나를 반환
+        timeOfDay& operator++() {
+            minute++;
+            if (minute >= 60) {
+                minute = 0;
+                hour++;
+                if (hour >= 24) {
+                    hour = 0;
+                }
+            }
+            return *this; // 내 원본 객체 자체를 리턴
+
+        // ++minute;
+        // if (minute==60) {minute=0;++hour;}
+        // if (hour==24){hour=0;}
+        // return timeOfDay{hour, minute};
+        }
+
+        // 2. 후위 증가 연산자 (t++) : 현재 상태를 복사해두고, 1분 더한 뒤, 복사본 반환
+        // (괄호 안에 의미 없는 'int'를 적어주는 게 전위랑 구별하는 C++의 약속!)
+        timeOfDay operator++(int) {
+            timeOfDay temp = *this; // 바뀌기 전 현재 상태 저장 (복사생성자 작동)
+            
+            // 위에 만든 전위 증가 함수를 재활용해서 1분 올리기
+            ++(*this); 
+            
+            return temp; // 1분 올라가기 전의 옛날 상태를 리턴
+
+        // timeOfDay temp{hour,minute};
+        // ++minute;
+        // if (minute==60) {minute=0;++hour;}
+        // if (hour==24){hour=0;}
+        // return temp;
+
+        }
+
+        // 3. 입력 연산자 (>>): std::cin >> t; 할 때 작동
+        friend std::istream& operator>>(std::istream& in, timeOfDay& t) {
+            int h, m;
+            in >> h >> m;
+            t.testHour(h);
+            t.testMinute(m);
+            t.hour = h;
+            t.minute = m;
+            return in;
+        // std::cout<<"Enter hour(0~23): "; in>>t.hour; t.testHour();
+        // std::cout<<"Enter minute(0~59): "; in>>t.minute; t.testMinute();
+        // return is;
+
+        }
+
+        // 4. 출력 연산자 (<<): std::cout << t; 할 때 작동
+        friend std::ostream& operator<<(std::ostream& out, const timeOfDay& t) {
+            if (t.hour < 10) out << "0";
+            out << t.hour << ":";
+            if (t.minute < 10) out << "0";
+            out << t.minute;
+            return out;
+            
+
+        }
+
+        // 5. 이항 연산자 (==): 두 시간이 같은지 비교
+        friend bool operator==(const timeOfDay& t1, const timeOfDay& t2) {
+            return (t1.hour == t2.hour) && (t1.minute == t2.minute);
+        }
+
+        // 6. 이항 연산자 (+): 두 시간을 더한 새로운 객체 반환
+        friend timeOfDay operator+(const timeOfDay& t1, const timeOfDay& t2) {
+            int totalMinute1 = t1.minute + t2.minute;
+            int totalHours = t1.hour + t2.hour + (totalMinutes / 60);
+            
+            totalMinutes %= 60;
+            totalHours %= 24; // 24시가 넘어가면 하루 순환 처리
+
+        // int minute1{t1.minute+t1.hour*60};
+        // int minute2{t2.minute+t2.hour*60};
+        // int totalMinutes{minute1+minute2};
+        // int newMinute{totalMinutes%60};
+        // int newHour{totalMinte/60};
+        // if (newHour >24)newHour = newHour%24;
+        // return timeOfDay{newHour, newMinute};
+            
+            return timeOfDay(totalHours, totalMinutes);
+        }
+    };
 }
