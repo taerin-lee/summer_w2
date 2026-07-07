538344f8ce21218962e28fa545e4cb63a8c20f01
taerin-lee <flsxodl@ewhain.net>
2026-07-07 02:43:24 +0000

    lab6

--- alarm.h
+++ alarm.h
@@ -1,26 +1,55 @@
 #pragma once
 #include <iostream>
+#include <string>
 #include "timeOfDay.h"
 
 namespace LeeTaerin2276249 {
 
     class alarm {
     private:
+        std::string name;
         timeOfDay wakeTime;
         bool inActive; 
 
     public:
-
-        alarm(timeOfDay t = timeOfDay(0, 0), bool a = false) : wakeTime(t), inActive(a) {}
+        alarm(const std::string& n = "no name yet", timeOfDay t = timeOfDay(0, 0), bool a = false) 
+            : name{n}, wakeTime(t), inActive(a) {}
 
         void print() const {
+            std::cout << name << ": ";
             wakeTime.print();
             std::cout << " alarm is " << (inActive ? "on" : "off") << "\n";
         }
-
-        timeOfDay& getWakeTime() {
-            return wakeTime;
+        friend std::ostream& operator<<(std::ostream& os, const alarm& a) {
+            os << a.name << ": ";
+            os << a.wakeTime; 
+            os << " alarm is " << ((a.inActive) ? "on" : "off") << '\n';
+            return os;
         }
+        void input() {
+            std::cout << "Enter alarm name: ";
+            std::getline(std::cin >> std::ws, name); 
+            wakeTime.input();
+            std::cout << "Enter 1 or 0 (on/off): ";
+            std::cin >> inActive;
+        }
+            friend std::istream& operator>>(std::istream& is, alarm& a) {
+            std::cout << "Enter alarm name: ";
+            std::getline(is >> std::ws, a.name); 
+            is >> a.wakeTime;
+            std::cout << "Enter 1 or 0 (on/off): ";
+            is >> inActive;
+            return is;
+        }
+
+        const std::string& getName() const {return wakeTime;}
+        const timeOfDay& getWakeTime() const { return wakeTime; }
+        void setName(const std::string& n) { name = n; }
+        void setWakeTime(const timeOfDay& t) { wakeTime = t; }
+        bool getActive() const { return inActive; }
+        void setActive(bool b) { inActive = b; }
+
+
     };
 
 }
--- main.cpp
+++ main.cpp
@@ -1,41 +1,39 @@
 #include <iostream>
-#include "timeOfDay.h"
+#include <string>
+#include <array>
 #include "alarm.h"
 
-int main() {
+namespace LeeTaerin2276249 {
 
-    using namespace LeeTaerin2276249;
-    using namespace std;
-
-    timeOfDay t1, t2;
-    // 1. 객체 2개를 입력연산자(>>)를 이용해서 표준스트림으로 입력
-    // (터미널에 입력할 때 예: 10 20 엔터 치면 10시 20분으로 들어감)
-    std::cin>> t1;
-    std::cin>> t2;
-
-    // 2. 출력연산자(<<)를 이용해서 표준스트림으로 출력
-    cout << t1 << "\n";
-    cout << t2 << "\n";
-    // std::cout <<t1 <<" "<< t2 <<'\n';
-
-    // 3. 두번째 객체에 전위증가연산자(++) 적용한 것을 표준스트림으로 출력
-    cout << ++t2 << "\n";
-
-    // 4. 두번째 객체에 후위증가연산자(++) 적용한 것을 표준스트림으로 출력
-    cout << t2++ << "\n";
-
-    // 5. 두번째 객체를 표준스트림으로 출력 (후위증가 이후 반영된 결과 확인용)
-    cout << t2 << "\n";
-
-    // 6. 두 객체를 비교연산자(==)를 이용하여 비교
-    if (t1 == t2) {
-        cout << "same\n";
-    } else {
-        cout << "different\n";
+    void printAlarmArray(const alarm arr[], const int n) {
+        for (int i = 0; i < n; ++i) {
+            std::cout << arr[i] << "\n";
+        }
     }
 
-    // 7. 두 객체를 +이항연산자를 이용하여 더한 값을 표준스트림으로 출력
-    cout << (t1 + t2) << "\n";
-    // std::cout << t1+t2 <<std::endl;
+}
+
+int main() {
+    using namespace LeeTaerin2276249;
+    const int n{4};
+
+    alarm alarmArray[n];
+    alarmArray[0] = alarm{"Morning", {6, 30}, 1};
+    alarmArray[1].setName("Class");
+    alarmArray[1].setWakeTime(timeOfDay(9, 0));
+    alarmArray[1].setActive(1);
+    alarmArray[2].input();
+    std::cin >> alarmArray[3];
+
+    printAlarmArray(alarmArray, n);
+
+    std::array<alarm, n> alarmStdArray;
+
+    for (int i = 0; i < alarmStdArray.size(); ++i) {
+        alarmStdArray.at(i) = alarmArray[i];
+    }
+    for (const auto& alm : alarmStdArray) {
+        alm.print();
+    }
     return 0;
 }
